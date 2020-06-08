67fa15fcd744b3cc750fe0cceb68ce91b5dfb42c
Varun Gupta <varun@gnxgroup.org>
2020-06-08 12:58:11 +0530

    Update node.sh

--- node.sh
+++ node.sh
@@ -4,5 +4,7 @@ sudo apt-get update
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
+sudo apt-get install -y build-essential
+
 sudo npm install n
 sudo n 12.14.1
