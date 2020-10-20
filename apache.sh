3540541aa84a6047c1cb5b7edd194fec4f2ba231
Varun Gupta <varun@gnxgroup.org>
2020-10-20 23:42:38 +0530

    Update apache.sh

--- apache.sh
+++ apache.sh
@@ -1,3 +1,27 @@
 sudo apt update && sudo apt upgrade
-sudo apt install -y tasksel
-sudo tasksel install lamp-server
+
+sudo systemctl stop nginx.service
+
+sudo apt install -y apache2
+sudo ufw allow in "Apache Full"
+
+sudo apt install -y curl
+sudo apt install -y mysql-server
+
+
+sudo mysql_secure_installation
+
+sudo apt install -y php libapache2-mod-php php-mysql
+sudo apt install -y php-curl php-gd php-mbstring php-xml php-xmlrpc php-soap php-intl php-zip
+
+sudo a2enmod rewrite
+sudo apache2ctl configtest
+
+#sudo systemctl restart apache2
+curl -O https://wordpress.org/latest.tar.gz
+tar xzvf latest.tar.gz
+
+
+sudo systemctl restart apache2
+
+
