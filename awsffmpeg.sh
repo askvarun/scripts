7345e176cae6653c213aee43aed597591367e3c7
Varun Gupta <varun@gnxgroup.org>
2021-02-07 04:09:45 +0530

    Create awsffmpeg.sh

---
+++ awsffmpeg.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt-get update
+
+sudo snap install ffmpeg
+
+sudo apt install unzip
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
