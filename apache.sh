587a86ed652709ac2ea377e06aa2b67d8550f820
Varun Gupta <varun@gnxgroup.org>
2021-05-03 17:12:50 +0530

    Update apache.sh

--- apache.sh
+++ apache.sh
@@ -14,6 +14,9 @@ sudo mysql_secure_installation
 #sudo apt install -y php libapache2-mod-php php-mysql
 #sudo apt install -y php-curl php-gd php-mbstring php-xml php-xmlrpc php-soap php-intl php-zip
 
+sudo apt install python-software-properties
+sudo add-apt-repository ppa:ondrej/php
+
 sudo apt install php7.4 libapache2-mod-php
 sudo apt install php7.4-gd php7.4-mysql php7.4-cli php7.4-xml php7.4-mysql php7.4-memcache php7.4-memcached php7.4-zip
 sudo apt install php7.4-xmlrpc php7.4-tidy php7.4-tideways php7.4-redis php7.4-mbstring php7.4-mcrypt php7.4-json php7.4-imagick php7.4-gmp
