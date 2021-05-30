48d5fa056c20bfd37521b9205390e6bcc07a471a
Varun Gupta <varun@gnxgroup.org>
2021-05-31 03:24:13 +0530

    php fpm

--- apache.sh
+++ apache.sh
@@ -20,7 +20,7 @@ sudo apt install php
 sudo apt install software-properties-common
 sudo add-apt-repository ppa:ondrej/php
 
-sudo apt install php7.4 libapache2-mod-php
+sudo apt install php7.4 libapache2-mod-php php7.4-fpm
 sudo apt install php7.4-gd php7.4-mysql php7.4-cli php7.4-xml php7.4-mysql php7.4-memcache php7.4-memcached php7.4-zip
 sudo apt install php7.4-xmlrpc php7.4-tidy php7.4-tideways php7.4-redis php7.4-mbstring php7.4-mcrypt php7.4-json php7.4-imagick php7.4-gmp
 
