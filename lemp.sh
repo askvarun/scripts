c9e25aff9d0db681b36df100509cac44b450ca2e
Varun Gupta <varun@gnxgroup.org>
2021-08-21 01:06:29 +0530

    Update lemp.sh

--- lemp.sh
+++ lemp.sh
@@ -1,1 +1,11 @@
+sudo apt update
+sudo apt install nginx
+
+sudo apt install mysql-server
+sudo mysql_secure_installation
+
+sudo add-apt-repository universe
+
+sudo apt install php-fpm php-mysql
+
 
