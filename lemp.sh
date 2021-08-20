388f77b9eaa3c1653633d120c33173357aaa7a98
Varun Gupta <varun@gnxgroup.org>
2021-08-21 01:47:20 +0530

    Update lemp.sh

--- lemp.sh
+++ lemp.sh
@@ -8,4 +8,10 @@ sudo add-apt-repository universe
 
 sudo apt install php-fpm php-mysql
 
+sudo apt install php-curl php-gd php-intl php-mbstring php-soap php-xml php-xmlrpc php-zip
+sudo systemctl restart php7.4-fpm
+
+curl -LO https://wordpress.org/latest.tar.gz
+tar xzvf latest.tar.gz
+
 
