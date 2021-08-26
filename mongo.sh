7c9b633f9f9a3fdc099701e84af71614fe6f98e0
Varun Gupta <varun@gnxgroup.org>
2021-08-26 17:51:38 +0530

    Update mongo.sh

--- mongo.sh
+++ mongo.sh
@@ -4,7 +4,9 @@ sudo apt-get update
 sudo apt-get install gnupg
 wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
 
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/5.0 multiverse" | sudo tee /etc/a$
+# for ubuntu 20.04
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
+
 
 sudo apt-get update
 sudo apt-get install -y mongodb-org
