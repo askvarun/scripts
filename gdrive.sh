e57b02961015705d42b5f562eb3de2d3b66bf087
Varun Gupta <varun@gnxgroup.org>
2021-08-28 17:41:42 +0530

    Create gdrive.sh

---
+++ gdrive.sh
@@ -0,0 +1,8 @@
+# https://github.com/prasmussen/gdrive
+# https://github.com/prasmussen/gdrive/releases/tag/2.1.1
+# https://blog.ruanbekker.com/blog/2020/05/08/server-backups-to-google-drive-using-the-drive-cli-tool/
+wget https://github.com/prasmussen/gdrive/releases/download/2.1.1/gdrive_2.1.1_linux_386.tar.gz
+tar -xf gdrive_2.1.1_linux_386.tar.gz
+sudo mv gdrive /usr/bin/
+chmod +x /usr/bin/gdrive
+# gdrive about
