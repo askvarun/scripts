5bcbcefbaa617a614a5b888e0dc91c2614e6e967
Varun Gupta <varun@gnxgroup.org>
2021-08-28 23:59:36 +0530

    Create getAll.sh

---
+++ getAll.sh
@@ -0,0 +1,11 @@
+wget https://raw.githubusercontent.com/askvarun/scripts/master/node.sh
+sudo chmod +x node.sh
+
+wget https://raw.githubusercontent.com/askvarun/scripts/master/lemp.sh
+sudo chmod +x lemp.sh
+
+wget https://raw.githubusercontent.com/askvarun/scripts/master/mongo.sh
+sudo chmod +x mongo.sh
+
+wget https://raw.githubusercontent.com/askvarun/scripts/master/gdrive.sh
+sudo chmod +x gdrive.sh
