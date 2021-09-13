939676bf18b1722017d2b1e0713357652b62ffb5
Varun Gupta <varun@gnxgroup.org>
2021-09-14 00:41:38 +0530

    Update node.sh

--- node.sh
+++ node.sh
@@ -15,7 +15,7 @@ sudo apt-get install -y speedtest-cli
 sudo apt-get install -y python3
 sudo apt-get install -y python3-pip
 sudo apt-get install -y libvips-dev
-sudo apt-get install python3-certbot-nginx
+sudo apt-get install -y python3-certbot-nginx
 
 #pip3 install bpytop
 
