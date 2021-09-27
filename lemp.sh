5ba7adaf6a0404a96dae79e39e3350f40d2c23b8
Varun Gupta <varun@gnxgroup.org>
2021-09-27 20:58:56 +0530

    Update lemp.sh

--- lemp.sh
+++ lemp.sh
@@ -1,21 +1,21 @@
 sudo apt update
-sudo apt install nginx
+sudo apt install -y nginx
 
-sudo apt install mysql-server
+sudo apt install -y mysql-server
 sudo mysql_secure_installation
 
-sudo apt install memcached
-sudo apt install libmemcached-tools
+sudo apt install -y memcached
+sudo apt install -y libmemcached-tools
 # sudo nano /etc/memcached.conf
 sudo systemctl restart memcached
 
 
 sudo add-apt-repository universe
 
-sudo apt install php-fpm php-mysql php-memcached php-cli
-sudo apt-get install python3-certbot-nginx
+sudo apt install -y php-fpm php-mysql php-memcached php-cli
+sudo apt install -y python3-certbot-nginx
 
-sudo apt install php-curl php-gd php-intl php-mbstring php-soap php-xml php-xmlrpc php-zip
+sudo apt install -y php-curl php-gd php-intl php-mbstring php-soap php-xml php-xmlrpc php-zip
 sudo systemctl restart php7.4-fpm
 
 sudo chown -R $USER:$USER /var/www/
